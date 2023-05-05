44a32544b5d3fb7ef46f1fa9e19b76b6dc131b43
Prathima Banasode <prathimabanasode5.pb@gmail.com>
2023-05-05 14:26:41 +0530

    Hotstar

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -14,8 +14,12 @@ public class ProductionHouseService {
     ProductionHouseRepository productionHouseRepository;
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
+        ProductionHouse productionHouse = new ProductionHouse();
+        productionHouse.setName(productionHouseEntryDto.getName());
+        productionHouse.setRatings(0);
+        ProductionHouse savedProductionHouse = productionHouseRepository.save(productionHouse);
 
-        return  null;
+        return savedProductionHouse.getId();
     }
 
 

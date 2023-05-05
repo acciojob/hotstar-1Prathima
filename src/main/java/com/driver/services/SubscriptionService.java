0c3007af76c55b2743e8eabcaa84f0d1295f14d8
Prathima Banasode <prathimabanasode5.pb@gmail.com>
2023-05-05 14:58:17 +0530

    p

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -86,39 +86,43 @@ public class SubscriptionService {
         //Hint is to use findAll function from the SubscriptionDb
 
         List<Subscription> subscriptionList = subscriptionRepository.findAll();
-        List<Subscription> subscriptionsForBasic = new ArrayList<>();
-        List<Subscription> subscriptionsForPro = new ArrayList<>();
-        List<Subscription> subscriptionsForElite = new ArrayList<>();
-
+        int totalAmount = 0;
         for(Subscription subscription : subscriptionList){
-            if(subscription.getSubscriptionType().equals(SubscriptionType.BASIC)){
-                subscriptionsForBasic.add(subscription);
-            }
-            else if(subscription.getSubscriptionType().equals(SubscriptionType.PRO)){
-                subscriptionsForPro.add(subscription);
-            }
-            else{
-                subscriptionsForElite.add(subscription);
-            }
+            totalAmount += subscription.getTotalAmountPaid();
         }
+//        List<Subscription> subscriptionsForBasic = new ArrayList<>();
+//        List<Subscription> subscriptionsForPro = new ArrayList<>();
+//        List<Subscription> subscriptionsForElite = new ArrayList<>();
+//
+//        for(Subscription subscription : subscriptionList){
+//            if(subscription.getSubscriptionType().equals(SubscriptionType.BASIC)){
+//                subscriptionsForBasic.add(subscription);
+//            }
+//            else if(subscription.getSubscriptionType().equals(SubscriptionType.PRO)){
+//                subscriptionsForPro.add(subscription);
+//            }
+//            else{
+//                subscriptionsForElite.add(subscription);
+//            }
+//        }
+//
+//        int basicAmount = 0;
+//        int proAmount = 0;
+//        int eliteAmount = 0;
+//
+//        for(Subscription subscription : subscriptionsForBasic){
+//            basicAmount += subscription.getTotalAmountPaid();
+//        }
+//        for(Subscription subscription : subscriptionsForPro){
+//            proAmount += subscription.getTotalAmountPaid();
+//        }
+//        for(Subscription subscription : subscriptionsForElite){
+//            eliteAmount += subscription.getTotalAmountPaid();
+//        }
+//
+//        int revenue = (basicAmount*subscriptionsForBasic.size()) + (proAmount*subscriptionsForPro.size()) + (eliteAmount*subscriptionsForElite.size());
 
-        int basicAmount = 0;
-        int proAmount = 0;
-        int eliteAmount = 0;
-
-        for(Subscription subscription : subscriptionsForBasic){
-            basicAmount += subscription.getTotalAmountPaid();
-        }
-        for(Subscription subscription : subscriptionsForPro){
-            proAmount += subscription.getTotalAmountPaid();
-        }
-        for(Subscription subscription : subscriptionsForElite){
-            eliteAmount += subscription.getTotalAmountPaid();
-        }
-
-        int revenue = (basicAmount*subscriptionsForBasic.size()) + (proAmount*subscriptionsForPro.size()) + (eliteAmount*subscriptionsForElite.size());
-
-        return revenue*subscriptionList.size();
+        return totalAmount*subscriptionList.size();
     }
 
 }
